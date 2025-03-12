080666d2b654dcf7836c42c8cc81a7220e615733
iyed-zouari <173332947+iyed-zouari@users.noreply.github.com>
2025-03-12 14:48:28 +0100

    Update EquipeTest.java

--- src/main/java/com/iyed/test/EquipeTest.java
+++ src/main/java/com/iyed/test/EquipeTest.java
@@ -12,6 +12,10 @@ public class EquipeTest {
 		e.setNomTerrain("Santiago Bernabéu");
 		e.setBudgetAnnuel(100023);
 		
+		Equipe e2 = new Equipe();
+		e2.setNomEquipe("Barcelona");
+		e2.setNomTerrain("Camp nou");
+		e2.setBudgetAnnuel(300200);
 		
 		//ajouter l'objet equipe à la BD
 		
@@ -19,14 +23,17 @@ public class EquipeTest {
 		cltDao.ajouter(e);
 		System.out.println("Appel de la méthode listerTous");
 		for (Equipe el : cltDao.listerTous())
-			System.out.println(el.getIdEquipe() + " " + el.getNomEquipe());
-		
+			System.out.println(el);
+		System.out.println("modification");
+		cltDao.modifier(e2);
+		System.out.println("suppression");
+		cltDao.supprimer(e);
 		
 		System.out.println("Appel de la méthode listerParNom");
 		
 		
-		for (Equipe el : cltDao.listerParNom("real"))
-			System.out.println(el.getIdEquipe()  + " " + el.getNomEquipe());
+		for (Equipe el : cltDao.listerParNom("r"))
+			System.out.println(el);
 		// tester les autres méthodes de la classe ClientDao
 	}
 }
